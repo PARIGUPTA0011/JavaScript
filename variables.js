112ae05621b1d2853a44a16c93cd0685fc4eb051
PARIGUPTA0011 <parigupta9667@gmail.com>
2025-09-07 18:19:58 +0530

    Added variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = '123456789012';
+let accountEmail = 'pari@gamil.com'
+var accountPassword = 'mySecretPassword';
+accountCity = 'Bangalore';
+accountId = '9876543210'; // not allowed
+accountEmail = 'bx@gmail'
+accountPassword = 'myNewPassword'
+accountCity = 'Mumbai'
+console.table([accountEmail , accountPassword, accountCity, accountId]); 
+
+/*
+Prefer not to use var
+*/
