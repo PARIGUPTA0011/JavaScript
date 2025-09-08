7538aff42c72d0956d968cbb2de087f946e642b3
PARIGUPTA0011 <parigupta9667@gmail.com>
2025-09-08 17:14:00 +0530

    DataTypes

---
+++ JS/DataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert("hello")
+
+// null
+// BigInt
+// undefined
+// Symbol => unique identifier
+// Boolean
+// String
+
+
+// Object
+console.log(typeof undefined);
+console.log(typeof null);
+
