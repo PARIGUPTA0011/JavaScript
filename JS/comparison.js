4495a3e586f420d5ad5e5cc0c0c619b638aab03d
PARIGUPTA0011 <parigupta9667@gmail.com>
2025-09-09 14:46:49 +0530

    Added comparison.js

---
+++ JS/comparison.js
@@ -0,0 +1,17 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);    
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null >0);
+// console.log(null >=0); comaprison operator converts into number
+console.log(null ==0); //equality operator works diff
+
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined >=0);
--- JS/coversionOps.js
+++ JS/coversionOps.js
@@ -1,12 +1,17 @@
-let score = 33;
+// let score = 33;
 
-console.log(typeof score); 
+// console.log(typeof score); 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
 
 // "33" => 22
 // "33abc" => NaN (type of NaN is number)
 // true => 1 , false => 0
+
+let value = 3;
+let negValue = -value;
+console.log(typeof negValue);
+console.log(typeof value);
 
