58e532cc069674ae1ebcd9f82aac7c43dbb75079
PARIGUPTA0011 <parigupta9667@gmail.com>
2025-09-09 14:16:36 +0530

    Added Coversion

--- JS/DataTypes.js
+++ JS/DataTypes.js
@@ -14,3 +14,4 @@
 console.log(typeof undefined);
 console.log(typeof null);
 
+
---
+++ JS/coversionOps.js
@@ -0,0 +1,12 @@
+let score = 33;
+
+console.log(typeof score); 
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// "33" => 22
+// "33abc" => NaN (type of NaN is number)
+// true => 1 , false => 0
+
